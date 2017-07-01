52dfeba7b1c11bbef62090e2888782601a2eb117
Andrew Abalos <andysaurusrex21@gmail.com>
2017-07-01 16:35:20 -0700

    coding challenge 1

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// UDEMY
+// JAVASCRIPT
+// PRACTICE I
+// 1. John and friend invented a simple game where the player with the highest value of his height (cm) plus 5 times his age wins. ( There can be a draw)
+
+// 2. Create variable for height and varable for agegs of two friends and assign values
+
+// 3. Decide who wins and print the winner to console. Include score in the string that you output to the console.
+
+// BONUS: Add third player and decide who wins. (Needs && operator)
