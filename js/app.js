be96388c2860f653e5cd02c15408c333990b8351
Andrew Abalos <andysaurusrex21@gmail.com>
2017-07-03 12:49:16 -0700

    created variables and if else statement

--- js/app.js
+++ js/app.js
@@ -8,3 +8,18 @@
 // 3. Decide who wins and print the winner to console. Include score in the string that you output to the console.
 
 // BONUS: Add third player and decide who wins. (Needs && operator)
+
+var johnHeight = 170.18;
+var andrewHeight = 177.80;
+
+var johnAge = 20;
+var andrewAge = 21;
+
+var johnTotal = johnHeight + johnAge * 5;
+var andrewTotal = andrewHeight + andrewAge * 5;
+
+if (andrewTotal < johnTotal){
+  console.log("John wins the game!");
+} else {
+  console.log ("Andrew wins the game!");
+}
